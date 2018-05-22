bc491499666b9e974bd0adfdd33166d96cddb789
Alex Zhang <37784421+AlexGZhang2014@users.noreply.github.com>
2018-05-21 21:27:56 -0400

    create

---
+++ Simple Fun #176: Reverse Letter.js
@@ -0,0 +1,27 @@
+Instructions:
+
+Task
+Given a string str, reverse it omitting all non-alphabetic characters.
+
+Example
+For str = "krishan", the output should be "nahsirk".
+For str = "ultr53o?n", the output should be "nortlu".
+
+Input/Output
+[input] string str
+
+A string consists of lowercase latin letters, digits and symbols.
+
+[output] a string
+
+Solution:
+
+function reverseLetter(str) {
+  let array = [];
+  for (let letter in str) {
+    if (str.charCodeAt(letter) > 96 && str.charCodeAt(letter) < 123) {
+      array.push(str[letter]);
+    }
+  }
+  return array.reverse().join('');
+}
