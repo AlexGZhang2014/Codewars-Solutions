fee08245fb66bb49fb6e9ea1d9b449f564191d9f
Alex Zhang <alexgzhang92@gmail.com>
2018-05-29 21:24:05 -0400

    Create

---
+++ Sum of two lowest positive integers.rb
@@ -0,0 +1,17 @@
+Instructions:
+
+Create a function that returns the sum of the two lowest positive numbers given an array of minimum 4 integers.
+No floats or empty arrays will be passed.
+
+For example, when an array is passed like [19, 5, 42, 2, 77], the output should be 7.
+
+[10, 343445353, 3453445, 3453545353453] should return 3453455.
+
+Hint: Do not modify the original array.
+
+Solution:
+
+def sum_two_smallest_numbers(numbers)
+  new_array = numbers.sort
+  sum = new_array[0] + new_array[1]
+end
